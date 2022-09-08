2847d7c690da5dfd3ce4dd71560849d11a8d385f
Maks-Bax <rn4aey@yandex.ru>
2022-09-08 18:46:07 +0300

    addChangeArtifacts

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,21 +1,21 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-class CallbackTest {
+public class CallbackTest {
     @Test
-    void shouldTest() {
+    public void shouldTest() throws InterruptedException {
         open("http://localhost:9999");
-        SelenideElement form = $("[class=\"App_appContainer__3jRx1\"]");
+        SelenideElement form = $(".form.form_size_m.form_theme_alfa-on-white");
         form.$("[type=\"text\"]").setValue("Коркина Света");
-        form.$(" [type=\"tel\"]").setValue("+79191234567");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[type=\"button\"]").click();
-        $("[data-test-id=\"order-success\"]").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        form.$("  [type=\"tel\"]").setValue("+79191234567");
+        form.$(".checkbox__box").click();
+        form.$("button").click();
+        $(".paragraph.paragraph_theme_alfa-on-white").shouldHave(Condition.exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
