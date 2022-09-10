4ea62d4b0f2968019eac28570cbffb8261f1db25
Maks-Bax <rn4aey@yandex.ru>
2022-09-11 00:17:44 +0300

    renameCssSelector

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -9,13 +9,13 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class CallbackTest {
     @Test
-    public void shouldTest() throws InterruptedException {
+    public void shouldTest() {
         open("http://localhost:9999");
         SelenideElement form = $(".form.form_size_m.form_theme_alfa-on-white");
         form.$("[type=\"text\"]").setValue("Коркина Света");
         form.$("  [type=\"tel\"]").setValue("+79191234567");
         form.$(".checkbox__box").click();
         form.$("button").click();
-        $(".paragraph.paragraph_theme_alfa-on-white").shouldHave(Condition.exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=\"order-success\"]").shouldHave(Condition.exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
